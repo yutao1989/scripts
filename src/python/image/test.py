0f16cf31a55f1981e954624c15048041bd27e2f5
yutao1989 <yutao@localhost.localdomain>
2017-07-26 20:08:41 +0800

    add image processor

--- src/python/image/test.py
+++ src/python/image/test.py
@@ -13,7 +13,7 @@ import random
 nbs = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 colors = [[255, 255, 0], [255, 204, 0], [255, 153, 0], [255, 102, 0], [255, 51, 0], [204, 255, 0], [204, 204, 0], [204, 153, 0], [204, 102, 0], [204, 51, 0], [102, 255, 0], [102, 0, 0], [51, 0, 102], [154, 50, 205], [127, 255, 0], [122, 103, 238], [34, 139, 34], [0, 100, 0]]
 
-conf = [.7,1]
+conf = [1.,1.2,1.3,1.3]
 
 def display(img):
     io.imshow(img)
@@ -81,7 +81,8 @@ def merge(grey_img,mp):
             print("----",g,n)
             merge_real(g,n,g2p,mp)
             merged.add(n)
-            n = find_neighbors(grey_img,g,g2p,mp)
+            #n = find_neighbors(grey_img,g,g2p,mp)
+            n = find_neighbors_by_mean(grey_img,g,g2p,mp)
 
 def distance(p1,p2):
     return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
@@ -91,19 +92,70 @@ def get_mean_std(lst):
     ys = [o[1] for o in lst]
     md = math.sqrt(np.std(xs)**2 + np.std(ys)**2)
     mean = (np.mean(xs), np.mean(ys))
-    return (mean,md)
+    mx = max([distance(o,mean) for o in lst])
+    return (mean,md,mx)
 
 def judge(d,g1,g2, mp):
-    m1,d1 = get_mean_std(mp[g1])
-    m2,d2 = get_mean_std(mp[g2])
+    m1,d1,mx1 = get_mean_std(mp[g1])
+    m2,d2,mx2 = get_mean_std(mp[g2])
+    m3,d3,mx3 = get_mean_std(mp[g1]+mp[g2])
     mean_dis = distance(m1,m2)
-    if (d1+d2+d)/mean_dis >= conf[0]:
+    #if (d1*conf[2]+d2*conf[3]+d)/mean_dis >= conf[1]:
+    if d2 == 0:
+        print(mp[g2])
+        return True
+    if d3/d2 < 1.2:
         return True
     else:
         return False
 
+def find_neighbors_by_mean(grey_img,g,mp1,mp2):
+    s = ([],{},{})
+    m_p,md,mx = get_mean_std(mp1[g])
+    shape = grey_img.shape
+    s[0].append(0)
+    s[1][0] = []
+    for item in mp1[g]:
+        d = distance(item,m_p)
+        if d not in s[1]:
+            bisect.insort(s[0],d)
+            s[1][d] = [item]
+        else:
+            s[1][d].append(item)
+        s[2][item] = item
+
+    while len(s[0]) > 0:
+        d = s[0].pop()
+        if d > mx*conf[0]:
+            break
+        items = s[1][d]
+        s[1].__delitem__(d)
+        for item in items:
+            for nb in nbs:
+                nx = item[0] + nb[0]
+                ny = item[1] + nb[1]
+                if nx >= 0 and ny >= 0 and nx < shape[0] and ny < shape[1]:
+                    pass
+                else:
+                    continue
+                if (nx,ny) not in s[2]:
+                    l = distance((nx,ny),m_p)
+                    if grey_img[nx][ny] == 255:
+                        if judge(l,g,mp2[(nx,ny)],mp1):
+                            return mp2[(nx,ny)]
+                        else:
+                            return None
+                    if l not in s[1]:
+                        bisect.insort(s[0],l)
+                        s[1][l] = [(nx,ny)]
+                        s[2][(nx,ny)] = s[2][item]
+                    else:
+                        s[1][l].append((nx,ny))
+                        s[2][(nx,ny)] = s[2][item]
+
 def find_neighbors(grey_img,g,mp1,mp2):
     s = ([],{},{})
+    _,md,mx = get_mean_std(mp1[g])
     shape = grey_img.shape
     s[0].append(0)
     s[1][0] = []
@@ -111,12 +163,9 @@ def find_neighbors(grey_img,g,mp1,mp2):
         s[1][0].insert(0,item)
         s[2][item] = item
 
-    _,md = get_mean_std(mp1[g])
-
     while len(s[0]) > 0:
         d = s[0].pop()
-        # todo: have to resize automatically
-        if d > md*conf[1]:
+        if d > md*conf[0]:
             break
         items = s[1][d]
         s[1].__delitem__(d)
